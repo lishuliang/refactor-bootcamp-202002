daee9a0b8ef1f55441f2d15aa43ca3174cbb05e2
shuliang.li <532784460@qq.com>
2020-02-14 20:35:04 +0800

    feat: add test should_return_3_when_calculate_given_position_is_4

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,13 +1,28 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import java.util.ArrayList;
+
 public class Fibonacci {
     private final int position;
+    private ArrayList<Integer> resultList;
 
     public Fibonacci(int position) {
         this.position = position;
+        resultList = new ArrayList<Integer>(){{
+            add(0);
+            add(1);
+            add(1);
+        }};
     }
 
-    public int calculate() {
-        return 1;
+    public void calculate() {
+
+        for (int i = 3; i <= position; i++) {
+            resultList.add(resultList.get(i - 2) + resultList.get(i - 1));
+        }
+    }
+
+    public int getFibonacci() {
+        return resultList.get(position);
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -1,16 +1,29 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FibonacciTest {
     @Test
     void should_return_1_when_calculate_given_position_is_1() {
         Fibonacci fibonacci = new Fibonacci(1);
+        fibonacci.calculate();
 
-        int calculate = fibonacci.calculate();
+        int result = fibonacci.getFibonacci();
 
-        Assert.assertEquals(1, calculate);
+        assertEquals(1, result);
     }
+
+    @Test
+    void should_return_3_when_calculate_given_position_is_4() {
+        Fibonacci fibonacci = new Fibonacci(4);
+        fibonacci.calculate();
+
+        int result = fibonacci.getFibonacci();
+
+        assertEquals(3, result);
+    }
+
 }
 
